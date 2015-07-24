df67a7e40bcc6c95fb6e90350070085b06a4f6ed
Stephen Bates <stephen.bates@pmcs.com>
2015-07-24 14:53:49 -0700

    More fixes to myfirstrdma.c

--- myfirstrdma/myfirstrdma.c
+++ myfirstrdma/myfirstrdma.c
@@ -49,26 +49,31 @@ enum errors {
     NO_BUFFER      = 4,
     NO_MR          = 5,
     NO_CONNECTION  = 6,
+    RUN_PROBLEM    = 7,
 };
 
-/* Define a container structure that stores all the relevant
- * information for this very simple RDMA program.
+/* 
+ * Define a container structure that stores all the relevant
+ * information for this very simple RDMA program. After that, assign
+ * some defaults to this container.
  */
 
 struct myfirstrdma {
-    char                    *server;
-	struct ibv_device       **dev_list;
-	struct ibv_context	    *ctx;
-    struct ibv_pd           *pd;
-    void                    *buf;
-    struct ibv_mr           *mr;
-    int                     mr_flags;
-    size_t                  size;
-    char                    *port;
-    struct rdma_addrinfo    hints;
-    struct rdma_cm_id       *lid;
-    struct rdma_cm_id       *cid;
-    struct ibv_qp_init_attr attr;
+  char                    *server;
+  struct ibv_device       **dev_list;
+  struct ibv_context	  *ctx;
+  struct ibv_pd           *pd;
+  void                    *buf;
+  struct ibv_mr           *mr;
+  int                     mr_flags;
+  size_t                  size;
+  char                    *port;
+  struct rdma_addrinfo    hints;
+  struct rdma_cm_id       *lid;
+  struct rdma_cm_id       *cid;
+  struct ibv_qp_init_attr attr;
+  unsigned                debug;
+  unsigned                verbose;
 };
 
 static const struct myfirstrdma defaults = {
@@ -78,9 +83,25 @@ static const struct myfirstrdma defaults = {
     .port     = "12345",
     .hints    = { 0 },
     .attr     = { 0 },
+    .debug    = 1,
+    .verbose  = 1,
 };
 
-/* A static (local) function to establish the rdma connection. If
+/* 
+ * A helper function to assist in debug when function calls return
+ * with an exit status.
+ */
+
+int __report(struct myfirstrdma *cfg, const char *func, int val)
+{
+  if (cfg->debug)
+    fprintf(stderr,"%s: %d = %s.\n", func, errno, strerror(errno));
+
+  return val;
+}
+
+/* 
+ * A static (local) function to establish the rdma connection. If
  * server is not NULL we know we are the client. This affects how
  * rdmacm is used. Return 0 on success.
  */
@@ -90,60 +111,86 @@ int __setup(struct myfirstrdma *cfg)
     int ret = 0;
     struct rdma_addrinfo *res;
 
-    fprintf(stderr, "%s\t%s\n", cfg->server, cfg->port);
+    /* 
+     * Use rdma_getaddrinfo to determine if there is a path to the
+     * other end. The server runs in passive mode so it waits for a
+     * connection. The client uses this function call to determine if
+     * a path exists based on the server name, port and the hints
+     * provided. The result comes back in res.
+     */
 
     cfg->hints.ai_port_space = RDMA_PS_TCP;
     if (cfg->server)
         ret = rdma_getaddrinfo(cfg->server, cfg->port, &cfg->hints, &res);
     else {
-        cfg->hints.ai_flags      = RAI_PASSIVE;
+        cfg->hints.ai_flags = RAI_PASSIVE;
         ret = rdma_getaddrinfo(NULL, cfg->port, &cfg->hints, &res);
     }
     if (ret)
-        return ret;
+      return __report(cfg, "rdma_getaddrinfo", ret);
 
-    cfg->attr.cap.max_send_wr     = cfg->attr.cap.max_recv_wr  = 1; 
-    cfg->attr.cap.max_send_sge    = cfg->attr.cap.max_recv_sge = 1; 
-    cfg->attr.cap.max_inline_data = 16; 
-    cfg->attr.sq_sig_all          = 1; 
-    if (cfg->server)
+    /* 
+     * Now create a communication identifier and (on the client) a
+     * queue pair (QP) for processing jobs. We set certain attributes
+     * on this link.
+     */
+
+    cfg->attr.cap.max_send_wr     = cfg->attr.cap.max_recv_wr  = 1;
+    cfg->attr.cap.max_send_sge    = cfg->attr.cap.max_recv_sge = 1;
+    cfg->attr.cap.max_inline_data = 16;
+    cfg->attr.sq_sig_all          = 1;
+
+    if (cfg->server){
       cfg->attr.qp_context = cfg->cid;
-
-    if (cfg->server)
       ret = rdma_create_ep(&cfg->cid, res, NULL, &cfg->attr);
-    else
+    } else
       ret = rdma_create_ep(&cfg->lid, res, NULL, &cfg->attr);
     
     if (ret)
-        return ret;
+        return __report(cfg, "rdma_create_ep", ret);
+
+    /* 
+     * We can now free the temporary variable with the address
+     * information for the other end of the link.
+     */
+
+    
     rdma_freeaddrinfo(res);
 
-    if (cfg->server){
-	    ret = rdma_connect(cfg->cid, NULL);
-	    if (ret){
-		    fprintf(stderr,"%s\n",strerror(errno));
-		    return ret;
-	    }
-		    
-        fprintf(stdout, "Client established a connection to %s.\n",
-                cfg->server);
-    } else {
-        ret = rdma_listen(cfg->lid, 0);
-        if (ret)
-            return ret;
-        ret = rdma_get_request(cfg->lid, &cfg->cid);
-	if (ret){
-	  fprintf(stderr,"%s\n",strerror(errno));
-	  return ret;
-	}
-        ret = rdma_accept(cfg->cid, NULL);
+    /* Now either connect to the server or setup a wait for a
+     *  connection from a client.
+     */
 
-        fprintf(stdout, "Server detected a connection.\n");
-	while(1)
-	  usleep(1000);
+    if (cfg->server){
+      ret = rdma_connect(cfg->cid, NULL);
+      if (ret)
+	return __report(cfg, "rdma_connect", ret);
+      if (cfg->verbose)
+	fprintf(stdout, "Client established a connection to %s.\n",
+		cfg->server);
+    } else {
+      ret = rdma_listen(cfg->lid, 0);
+      if (ret)
+	return __report(cfg, "rdma_listen", ret);
+      ret = rdma_get_request(cfg->lid, &cfg->cid);
+      if (ret)
+	return __report(cfg, "rdma_get_request", ret);
+      ret = rdma_accept(cfg->cid, NULL);
+      if (ret)
+	return __report(cfg, "rdma_accept", ret);
+      if (cfg->verbose)
+	fprintf(stdout, "Server detected a connection on %s from TBD.\n",
+		cfg->cid->verbs->device->name);
     }
 
     return ret;
+}
+
+int __run(struct myfirstrdma *cfg)
+{
+
+  usleep(10000);
+  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -154,10 +201,10 @@ int main(int argc, char *argv[])
     /* Process the command line. The only argument allowed is in
        client mode and it points to the server. */
 
-	if (argc>2)
-		return 1;
+    if (argc>2)
+      return 1;
     else if (argc==2)
-        cfg.server = strdup(argv[1]);
+      cfg.server = strdup(argv[1]);
 
     /* Now establish a RDMA connection between the client and
      * server. If we are the server we start and wait for the
@@ -166,9 +213,9 @@ int main(int argc, char *argv[])
      * the machine.
      */
 
-	cfg.dev_list = ibv_get_device_list(NULL);
-	if (!cfg.dev_list)
-		return NO_RDMA_DEV;
+    cfg.dev_list = ibv_get_device_list(NULL);
+    if (!cfg.dev_list)
+      return NO_RDMA_DEV;
 
     /* Now that we have found a RDMA port we set that up for a
      * connection by creates a context, a Protection Domain (PD), a
@@ -176,21 +223,21 @@ int main(int argc, char *argv[])
      * memory region for the MR via a call to malloc.
      */
 
-	cfg.ctx = ibv_open_device(cfg.dev_list[0]);
-	if (!cfg.ctx)
-        return NO_CONTEXT;
-
-	cfg.pd = ibv_alloc_pd(cfg.ctx);
-	if (!cfg.pd)
-		return NO_PROT_DOMAIN;
-
+    cfg.ctx = ibv_open_device(cfg.dev_list[0]);
+    if (!cfg.ctx)
+      return __report(&cfg, "ibv_open_device", NO_CONTEXT);
+    
+    cfg.pd = ibv_alloc_pd(cfg.ctx);
+    if (!cfg.pd)
+      return __report(&cfg, "ibv_alloc_pd", NO_PROT_DOMAIN);
+    
     cfg.buf = malloc(cfg.size);
-	if (!cfg.buf)
-		return NO_BUFFER;
-
-	cfg.mr = ibv_reg_mr(cfg.pd, cfg.buf, cfg.size, cfg.mr_flags);
-	if (!cfg.mr)
-		return NO_MR;
+    if (!cfg.buf)
+      return __report(&cfg, "malloc", NO_BUFFER);
+    
+    cfg.mr = ibv_reg_mr(cfg.pd, cfg.buf, cfg.size, cfg.mr_flags);
+    if (!cfg.mr)
+      return __report(&cfg, "ibv_reg_mr", NO_MR);
 
     /* Now we get to some client/server specifc code. We use the rdma
      * connection manager (rdmacm) library to establish a
@@ -200,7 +247,11 @@ int main(int argc, char *argv[])
      */
 
     if ( __setup(&cfg) )
-        return NO_CONNECTION;
+      return __report(&cfg, "setup", NO_MR);
+
+    if ( __run(&cfg) )
+      return __report(&cfg, "run", RUN_PROBLEM);
+
 
     /* Tear everything down in reverse order to how it was constucted
      * so we leave the program in a clean state.
